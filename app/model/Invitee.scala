4fae55e5b24b2fa2ea1d48977c0803129db42e9d
wardziniak <bwardziniak@yahoo.pl>
2015-07-26 15:58:59 +0200

    Json Reads/Writes

--- app/model/Invitee.scala
+++ app/model/Invitee.scala
@@ -1,5 +1,9 @@
 package model
 
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
 /**
  * Created by wardziniak on 26.07.15.
  */
@@ -9,6 +13,15 @@ object Invitee {
 
   var invitees = List(Invitee("Ludwig von Mises", "ludwig.mises@test.pl"), Invitee("Frédéric Bastiat", "frederic.bastiat@test.pl"))
 
-  def addInvitee(invitee: Invitee) = invitees = invitees ::: invitees
+  def addInvitee(invitee: Invitee) = invitees = invitees ::: List(invitee)
 
+  implicit val inviteeWriters: Writes[Invitee] = (
+    (__ \ "invitee").write[String] and
+      (__ \ "email").write[String]
+    ) (unlift(Invitee.unapply))
+
+  implicit val inviteeReaders: Reads[Invitee] = (
+    (__ \ "invitee").read[String] and
+      (__ \ "email").read[String]
+    ) (Invitee.apply _)
 }
