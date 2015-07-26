ae8b1a4fae2c1dfc9499ab1d40dd09976be25266
wardziniak <bwardziniak@yahoo.pl>
2015-07-26 16:06:49 +0200

    rest method implementation

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,25 @@
 package controllers
 
+import model.Invitee._
+import play.api.libs.json.{JsError, Json}
 import play.api.mvc._
 
 object Application extends Controller {
 
-  def invitationsList() = TODO
+  def invitationsList() = Action {
+    Ok(Json.toJson(invitees))
+  }
 
-  def saveInvitation() = TODO
+  def saveInvitation() = Action(BodyParsers.parse.json)
+  { request =>
+    val i = request.body.validate[Invitee]
+    i.fold(
+      errors => {
+        BadRequest(Json.obj("message" -> JsError.toFlatJson(errors)))
+      },
+      invitee => {
+        addInvitee(invitee)
+        Created
+      })
+  }
 }
